ee5c8290e7e032d3da0d94f078da4fd0243497be
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-08-21 13:50:05 +0100

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,32 +1,14 @@
-#!/usr/bin/python3
-"""This method determines if all the boxes can be opened."""
-
-
 def canUnlockAll(boxes):
-    new_list = []
-    index_list = []
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
 
-    for box in boxes:
-        if len(box) == 0:
-            boxes.pop(boxes.index(box))
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
 
-        if len(box) == 1 and box not in new_list and box[0] <= len(boxes):
-            new_list.append(box[0])
-
-        elif len(box) > 1:
-            for key in box:
-                if key > len(box):
-                    continue
-                elif key <= len(boxes) and key not in new_list:
-                    new_list.append(key)
-
-    for box in boxes:
-        if boxes.index(box)+1 not in index_list:
-            index_list.append(boxes.index(box)+1)
-
-    new_list.sort()
-    index_list.sort()
-
-    if new_list == index_list:
-        return True
-    return False
+    return all(visited)

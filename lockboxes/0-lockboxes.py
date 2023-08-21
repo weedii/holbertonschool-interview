65046f2a7d62f19e1ec15dae21ae03ced393e199
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-08-21 10:34:13 +0100

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,20 +4,29 @@
 
 def canUnlockAll(boxes):
     new_list = []
+    index_list = []
 
     for box in boxes:
-
         if len(box) == 0:
             boxes.pop(boxes.index(box))
+
         if len(box) == 1 and box not in new_list and box[0] <= len(boxes):
-            new_list.append(box)
+            new_list.append(box[0])
+
         elif len(box) > 1:
             for key in box:
-                if key > len(boxes):
-                    return False
-                elif key <= len(boxes) and box not in new_list:
-                    new_list.append(box)
+                if key > len(box):
+                    continue
+                elif key <= len(boxes) and key not in new_list:
+                    new_list.append(key)
 
-    if len(boxes) == len(new_list):
+    for box in boxes:
+        if boxes.index(box)+1 not in index_list:
+            index_list.append(boxes.index(box)+1)
+
+    new_list.sort()
+    index_list.sort()
+
+    if new_list == index_list:
         return True
     return False

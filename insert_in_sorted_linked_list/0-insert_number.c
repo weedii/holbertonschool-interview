bb9ba5e73e7a9e45fc2b73697babffa6fd9f74b0
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-14 15:34:44 +0100

    Insert in sorted linked list

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -15,6 +15,12 @@ listint_t *insert_node(listint_t **head, int number)
     new_node->n = number;
     new_node->next = NULL;
 
+    if (*head == NULL)
+    {
+        *head=new_node;
+        return new_node;
+    }
+
     current = *head;
     prev = NULL;
 

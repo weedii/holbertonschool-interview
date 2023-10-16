01ad39b269e8ce6625bb505fdd7dbcab45d5226b
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-16 14:31:37 +0100

    New node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
     if (new_node == NULL)
         return NULL;
+        
 
     new_node->parent = parent;
     new_node->n = value;

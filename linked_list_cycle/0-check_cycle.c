41c1fd8de14eb8b964210920291056dba291242e
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-17 13:50:20 +0100

    Linked list cycle

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks if
+ *		a singly linked list has a cycle in it.
+ * @list:  the linked list to be checked
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast = malloc(sizeof(list));
+
+	slow = list;
+	fast = list;
+
+	while ((slow != NULL && slow->next != NULL) &&
+		   (fast != NULL && fast->next != NULL))
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}

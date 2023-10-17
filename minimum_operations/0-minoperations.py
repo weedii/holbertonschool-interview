1ad0cdd82e760291319cfdafd1acb4dbe6460bef
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-17 12:35:33 +0100

    Minimum Operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """minOperations function"""
+    count = 0
+    string = 'H'
+    while len(string) < n:
+        if n % len(string) == 0:
+            string += string
+            count += 1
+        else:
+            string += string[0]
+            count += 1
+    if len(string) == n:
+        return count
+    else:
+        return 0
+
+
+"H" "HH" "HHH" "HHHHHH" "HHHHHHHHH"
+"H" "HH" ""
+"H" "HH" "HHH" "HHHHHH" "HHHHHHHHH" "HHHHHHHHHHHH"

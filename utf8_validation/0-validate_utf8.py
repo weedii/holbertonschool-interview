9b45da7fb9de3055d7ede0c7a4d1ced5637ffa92
weedii <abidiw293@gmail.com>
2024-09-03 13:07:15 +0100

    UTF-8 Validation

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -3,24 +3,25 @@
 validUTF8
 """
 
+
 def validUTF8(data):
     # number of bytes
     numBytes = 0
-    
+
     mask1 = 1 << 7  # 128
     mask2 = 1 << 6  # 64
-    
+
     for byte in data:
         # get the 8 least bits
         byte = byte & 255
-        
+
         if numBytes == 0:
             # check how many bytes
             mask = 1 << 7
             while mask & byte:
                 numBytes += 1
                 mask = mask >> 1
-            
+
             # check for 1 byte
             if numBytes == 0:
                 continue  # its a 1 byte character ASCII
@@ -30,8 +31,8 @@ def validUTF8(data):
             # check that the byte starts with '10'
             if not (byte & mask1 and not (byte & mask2)):
                 return False
-        
+
         # Decrement for each byte
         numBytes -= 1
-    
+
     return numBytes == 0

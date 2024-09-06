11852a767973e38f0aa9e5c8fef926e1209769f4
weedii <abidiw293@gmail.com>
2024-09-06 14:33:15 +0100

    AVL - From sorted array

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -8,18 +8,17 @@
  *
  * Return: Pointer to the new node or NULL on failure
  */
-avl_t *create_node(avl_t *parent, int value)
-{
-    avl_t *node = malloc(sizeof(avl_t));
-    if (!node)
-        return (NULL);
+avl_t * create_node(avl_t * parent, int value) {
+  avl_t * node = malloc(sizeof(avl_t));
+  if (!node)
+    return (NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
+  node -> n = value;
+  node -> parent = parent;
+  node -> left = NULL;
+  node -> right = NULL;
 
-    return (node);
+  return (node);
 }
 
 /**
@@ -32,20 +31,19 @@ avl_t *create_node(avl_t *parent, int value)
  *
  * Return: Pointer to the root of the AVL subtree
  */
-avl_t *build_avl_tree(int *array, int start, int end, avl_t *parent)
-{
-    if (start > end)
-        return (NULL);
+avl_t * build_avl_tree(int * array, int start, int end, avl_t * parent) {
+  if (start > end)
+    return (NULL);
 
-    int mid = (start + end) / 2;
-    avl_t *root = create_node(parent, array[mid]);
-    if (!root)
-        return (NULL);
+  int mid = (start + end) / 2;
+  avl_t * root = create_node(parent, array[mid]);
+  if (!root)
+    return (NULL);
 
-    root->left = build_avl_tree(array, start, mid - 1, root);
-    root->right = build_avl_tree(array, mid + 1, end, root);
+  root -> left = build_avl_tree(array, start, mid - 1, root);
+  root -> right = build_avl_tree(array, mid + 1, end, root);
 
-    return (root);
+  return (root);
 }
 
 /**
@@ -56,10 +54,9 @@ avl_t *build_avl_tree(int *array, int start, int end, avl_t *parent)
  *
  * Return: Pointer to the root node of the created AVL tree, or NULL on failure
  */
-avl_t *sorted_array_to_avl(int *array, size_t size)
-{
-    if (!array || size == 0)
-        return (NULL);
+avl_t * sorted_array_to_avl(int * array, size_t size) {
+  if (!array || size == 0)
+    return (NULL);
 
-    return (build_avl_tree(array, 0, size - 1, NULL));
+  return (build_avl_tree(array, 0, size - 1, NULL));
 }

dc2dfce663f626feb99b8d2ec13a03eca4e8a45d
weedii <abidiw293@gmail.com>
2024-09-08 13:39:19 +0100

    Star Wars Characters

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+import request from "request";
+
+const movieId = process.argv[2];
+console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+console.log(movieId);
+console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(movieUrl, (error, res, body) => {
+  if (error) {
+    console.error(`Error while fetching movie:${movieId}`, error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  characters.forEach((characterURL) => {
+    request(characterURL, (err, res, characterBody) => {
+      if (err) {
+        console.error(`Error while fetching character:${characterURL}`, err);
+        return;
+      }
+
+      const character = JSON.parse(characterBody);
+      console.log(character.name);
+    });
+  });
+});

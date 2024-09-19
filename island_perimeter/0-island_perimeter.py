b79185db312718b2c1124305643f1f7c0b8a9b6b
weedii <abidiw293@gmail.com>
2024-09-19 23:37:12 +0100

    Island Perimeter

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -16,19 +16,15 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                # top (i - 1)
                 if grid[i - 1][j] == 0:
                     perimeter += 1
 
-                # bottom (i + 1)
                 if grid[i + 1][j] == 0:
                     perimeter += 1
 
-                # left (j - 1)
                 if grid[i][j - 1] == 0:
                     perimeter += 1
 
-                # right (j + 1)
                 if grid[i][j + 1] == 0:
                     perimeter += 1
 

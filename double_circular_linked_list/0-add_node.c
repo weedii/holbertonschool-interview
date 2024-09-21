8a29ca9fc268ff5803684757e66a3020be0c18e2
weedii <abidiw293@gmail.com>
2024-09-21 14:09:29 +0100

    Add Nodes

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,81 @@
+#include "list.h"
+
+/**
+ * add_node_end - Function that adds a new node to the end
+ * of a double circular linked list
+ * @list: A pointer to the head of the list
+ * @str: The string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new_node, *last;
+
+	if (!list || !str)
+		return (NULL);
+
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = str;
+
+	if (!(*list))
+	{
+		/* List is empty */
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		/* List is not empty */
+		last = (*list)->prev;
+		new_node->next = *list;
+		new_node->prev = last;
+		last->next = new_node;
+		(*list)->prev = new_node;
+	}
+
+	return (new_node);
+}
+
+/**
+ * add_node_begin - Function that adds a new node to the beginning
+ * of a double circular linked list
+ * @list: A pointer to the head of the list
+ * @str: The string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new_node;
+
+	if (!list || !str)
+		return (NULL);
+
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = str;
+
+	if (!(*list))
+	{
+		/* List is empty */
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		/* List is not empty */
+		new_node->next = *list;
+		new_node->prev = (*list)->prev;
+		(*list)->prev->next = new_node;
+		(*list)->prev = new_node;
+		*list = new_node;
+	}
+
+	return (new_node);
+}

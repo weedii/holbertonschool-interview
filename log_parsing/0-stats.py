b6ab55964edc0f3928c8953551c152cb697603bc
weedii <abidiw293@gmail.com>
2024-09-22 14:57:52 +0100

    Log parsing

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Script that reads stdin line by line and computes metrics
+"""
+
+import sys
+
+FileSize = 0
+STATUS = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+count = 0
+
+
+def print_stats():
+    """Prints file size and status codes"""
+    print("File size: {}".format(FileSize))
+    for key, value in sorted(STATUS.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
+
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        if len(parts) >= 2:
+            try:
+                FileSize += int(parts[-1])
+            except ValueError:
+                pass
+
+            if parts[-2] in STATUS:
+                STATUS[parts[-2]] += 1
+
+        count += 1
+        if count % 10 == 0:
+            print_stats()
+finally:
+    print_stats()
